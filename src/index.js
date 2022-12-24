0a7dffb945c660795c9af73ef357cfe6bea39d63
Larry Lau <lsl1840@gmail.com>
2022-12-23 22:58:32 -0600

    deploy2

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
 );
 
