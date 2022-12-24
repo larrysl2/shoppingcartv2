078ef0fe190f4f1218d741b6a509aa972d924a87
Larry Lau <lsl1840@gmail.com>
2022-12-24 13:46:03 -0600

    deploy 11

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
@@ -9,9 +8,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
