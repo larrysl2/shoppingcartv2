4c8732451f130751733ff00c56fcb98da14be98f
Larry Lau <lsl1840@gmail.com>
2022-12-23 23:22:32 -0600

    deploy4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
    <Router>
       <Header/>
       <Routes>
-        <Route path= "/" element = {<Homepage/>}/>
+        <Route path= "/Homepage" element = {<Homepage/>}/>
         <Route path= "/Catalog" element = {<Catalog/>}/>
       </Routes>
    </Router>
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
